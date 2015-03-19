caec43d2c7a47a82d4e1f8627518c62ad92c850f
Robin Bate Boerop <me@robinbb.com>
2015-03-19 15:14:38 -0300

    Add prepend2path function to modules/default

--- modules/default/login.bash
+++ modules/default/login.bash
@@ -1,3 +1,10 @@
 add2path() {
    [[ ":$PATH:" =~ ":$1:" ]] || PATH=$1:${PATH#:}
 }
+prepend2path() {
+   PATH="$1${PATH:+:}$PATH"
+   local end=${PATH##*:$1}
+   local start=${PATH%:$1*}
+   [[ $start != $end ]] && end="$start$end"
+   PATH="$end"
+}

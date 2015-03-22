18240ce94b600c1c4c27e9ede89f69cafb215fd5
Robin Bate Boerop <me@robinbb.com>
2015-03-22 16:16:13 -0300

    Correct Nix MANPATH expression

--- modules/nix/interactive.sh
+++ modules/nix/interactive.sh
@@ -2,5 +2,5 @@ if [ -z "$BASHRC_D_NO_NIX" ] \
    && [ "$NIX_PATH" ]      \
    && [ -d "$HOME"/.nix-profile/share/man ]
 then
-   export MANPATH=~/.nix-profile/share/man:"$MANPATH"
+   export MANPATH=~/.nix-profile/share/man${MANPATH:+:}"$MANPATH"
 fi
